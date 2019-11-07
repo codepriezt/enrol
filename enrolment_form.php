800a135f9918c44503edabc0c3624dec730b03cc
codepriezt <kolumide22@gmail.com>
2019-11-07 13:19:00 +0100

    xmlrequest

--- enrolment_form.php
+++ enrolment_form.php
@@ -65,23 +65,23 @@ $fingerprint = strtolower(hash('sha512', $hashSequence));
 <p>&nbsp;</p>
 <p>
 	<form method="post" action="" >
-		<input type="hidden"  name="Username" value="<?php echo $username; ?>" />
-		<input type="hidden"  name="Password" value="<?php echo $password; ?>" />
-		<input type="hidden"  name="amount" value="<?php echo $amount; ?>" />
-		<input type="hidden"  name="description" value="<?php echo $productinfo; ?>" />
-		<input type="hidden"  name="safeKey" value="<?php echo $safeKey; ?>" />
-		<input type="hidden"  name="merchantReference" value="<?php echo $txnid; ?>" />
-		<input type="hidden"  name="transactionType" value="<?php echo $transactionType; ?>" />	
-		<input type="hidden"  name="returnUrl" value="<?php echo $returnUrl; ?>" />		
-		<input type="hidden"  name="cancelUrl" value="<?php echo $cancelUrl; ?>" />		
-		<input type="hidden"  name="firstname" value="<?php echo $USER->firstname; ?>" />
-		<input type="hidden"  name="email" value="<?php echo $USER->email; ?>" />
-		<input type="hidden"  name="phone" value="<?php echo $_SESSION['timestamp']; ?>" />
+		<input type="hidden" id="username" name="Username" value="<?php echo $username; ?>" />
+		<input type="hidden"  id="password" name="Password" value="<?php echo $password; ?>" />
+		<input type="hidden"  id="amount" name="amount" value="<?php echo $amount; ?>" />
+		<input type="hidden"  id="description" name="description" value="<?php echo $productinfo; ?>" />
+		<input type="hidden"  id="safeKey" name="safeKey" value="<?php echo $safeKey; ?>" />
+		<input type="hidden"  id="merchantReference" name="merchantReference" value="<?php echo $txnid; ?>" />
+		<input type="hidden"  id="transactionType" name="transactionType" value="<?php echo $transactionType; ?>" />	
+		<input type="hidden"  id="returnUrl" name="returnUrl" value="<?php echo $returnUrl; ?>" />		
+		<input type="hidden"  id="cancelUrl" name="cancelUrl" value="<?php echo $cancelUrl; ?>" />		
+		<input type="hidden"  id="firstname" name="firstname" value="<?php echo $USER->firstname; ?>" />
+		<input type="hidden"  id="email" name="email" value="<?php echo $USER->email; ?>" />
+		<input type="hidden"  id="phone" name="phone" value="<?php echo $_SESSION['timestamp']; ?>" />
 		<input type="hidden"  name="surl" value="<?php echo $CFG->wwwroot; ?>/enrol/payumoney/ipn.php" />
          <input type="hidden" name="udf1" value="<?php echo $udf1 ?>" />
 		<input type="hidden"  name="udf2" value="<?php echo $enrolperiod; ?>" />
 		
-		<input type="submit" id="sub_button" value="" />
+		<input type="submit" id="sub_button" value="" onClick="makeOrder" />
 	</form>
 </p>
 </div>
@@ -97,6 +97,53 @@ $fingerprint = strtolower(hash('sha512', $hashSequence));
   height: 59px;
 }
 </style>
+
+<script>
+ const btn = docunent.getElementById('sub_button').addEventListener('submit'  , makeOrder);
+
+
+ function makeOrder()
+ {	const username = document.querySelector('#username').value
+	const password = document.querySelector('#password').value
+	const amount = document.querySelector('#amount').value
+	const safeKey = document.querySelector('#safeKey').value
+	const description = document.querySelector('#description').value
+	const transactionType = document.querySelector('#transactionType').value
+	const firstname = document.querySelector('#firstname').value
+	const email = document.querySelector('#email').value
+	const phone = document.querySelector('#phone').value
+	const merchantReference = document.querySelector('#merchantReference').value
+	const returnUrl = document.querySelector('#returnUrl').value
+	const cancelUrl = document.querySelector('#cancelUrl').value
+
+	const data = {
+		username:username,
+		email:email,
+		amount:amount,
+		safeKey:safeKey,
+		description:description,
+		transactionType:transactionType,
+		password:password,
+		firstname:firstname,
+		merchantRefernce:merchantRefernce,
+		returnUrl:returnUrl,
+		cancelUrl:cancelUrl,
+		phone:phone
+	}
+
+	const http  = new XMLHttpRequest();
+	http.post('server.php' , data , function(err , res){
+		if(err){
+			console.log(err)
+		}
+		console.log(res)
+	})
+	 e.preventDefault();
+ }
+
+
+</script>
+
 
  
 	
--- server.php
+++ server.php
@@ -2,37 +2,9 @@
 require 'lib/nusoap.php';
 require_once 'enrolment_form.php';
 
-$server = new nusoap_server();
+try{
 
-$server->configureWSDL("soap request" ,"urn:soaprequest");
-
-$server->register(
-    'getResponse',
-    array("data"=>"xsd:array"),
-    array("return"=>"xsd:array")
-
-);
-
-function getResponse($TransactionResponse){
-    $res = $TransactionResponse;
-    
-    if($res->status == '0')
-    {
-        return $res ;
-    }else{
-        echo "unable to complete Process";
-
-    }
-    
-}
-
-$server->service(file_get_contents("php://input"));
-
-$client = new nusoap_client("https://staging.payu.co.za/service/PayUAPI?wsdl");
-
-
-if(isset($_POST['submit']))
-{
+    $Api = ONE_ZERO; 
     $username = $_POST['username'];
     $password = $_POST['password'];
     $amount = $_POST['amount'];
@@ -44,32 +16,35 @@ if(isset($_POST['submit']))
     $description = $_POST['description'];
     $cancelUrl = $_POST['cancelUrl'];
     $returnURL = $_POST['returnUrl'];
+    $merchantReference = $_POST['merchantRefernce'];
     $demoMode = true;
-    
 
-    $order = new stdClass();
+    $client = new nusoap_client("https://staging.payu.co.za/service/PayUAPI?wsdl");
 
-    $order->data = array(
-        $data->username = $username ,
-        $data->password = $password ,
-        $data->amount = $amount,
-        $data->safeKey = $safeKey,
-        $data->transactionType = $transactionType,
-        $data->firstname = $firstname ,
-        $data->email = $email ,
-        $data->phone = $phone ,
-        $data->description = $description ,
-        $data->cancelUrl = $cancelUrl,
-        $data->returnUrl = $returnUrl ,
-        $data->demoMode =$demoMode ,
-        $data->currency = "NGN"
-    );
+    $params = array('Api'=>$Api , 'username'=>$username , 'password'=>$password, 'amount'=>$amount , 'safeKey'=>$safeKey, 
+    'transactionType'=>$transactionType , 'firstname'=>$firstname , 'email'=>$email , 'phone'=>$phone ,'description'=>$description , 'cancelUrl'=>$cancelUrl,
+    'returnUrl'=>$returnUrl , 'merchantRefernce'=>$merchantReference , 'demoMode'=>$demoMode);
 
-    $client->call('getResponse' , array($order));
+    $response = $client->setTransaction($params);
 
-    
+    $array = json_decode(json_encode($response), true);
+
+    function getTransaction($Response){
+            if($Response->transactionState == 'successful'){
+                return "transaction successful";
+            }else{
+                echo "transactin not successful";
+            }
+            return $Response;
+      }
+     
+
+}catch(Exeception $e){
+
+    echo $e->getMessage();
+
+
+
 }
-
-	
 
 ?>
